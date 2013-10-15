c307846a914633ebbb86c3feef1a36c6fa18f931
pieteradejong <pieter.a.dejong@gmail.com>
2013-10-15 14:36:27 -0700

    changes

--- server/request-handler.js
+++ server/request-handler.js
@@ -3,7 +3,25 @@
  * basic-server.js.  So you must figure out how to export the function
  * from this file and include it in basic-server.js. Check out the
  * node module documentation at http://nodejs.org/api/modules.html. */
-
+// var http = require("http");
 var handleRequest = function(request, response) {
 
+  var defaultCorsHeaders = {
+    "access-control-allow-origin": "*",
+    "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
+    "access-control-allow-headers": "content-type, accept",
+    "access-control-max-age": 10 // Seconds.
+  };
+
+  console.log("Serving request type " + request.method + " for url " + request.url);
+  var statusCode = 200;
+  var headers = defaultCorsHeaders;
+  headers['Content-Type'] = "text/plain";
+  response.writeHead(statusCode, headers);
+  response.end("Hello, World! This is Pieter");
+
+
+  //'/1/classes/messages'
 };
+
+module.exports = handleRequest;
